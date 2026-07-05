29ba4255ba37643e1d170e57b5305c181ed1e08e
Shok-king <ayaan106643@gmail.com>
2026-07-05 22:20:18 +0100

    Add files via upload

--- Reg_NEWUSER.py
+++ Reg_NEWUSER.py
@@ -6,11 +6,11 @@ import numpy as np
 # Everything here will be a one time set up as they are the constructor method that needs to be initialised before the loop starts
 kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
 cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
-New = False
 #==========================
 #The while loop will constantly check for new frames from the kinect2 sensor, when that new frame is available it converts that picture into grey scale for image processing using haars cascade classifier.
-def MainLoop():
-    while not New:
+def get_face():
+
+    while True:
         if kinect.has_new_color_frame():
             raw_data = kinect.get_last_color_frame()
             raw_data = raw_data.reshape((1080, 1920, 4))
@@ -19,12 +19,19 @@ def MainLoop():
 
             face = cascade.detectMultiScale(image_GREY)
             if len(face) > 0:
-                New = True
-    file = open("database.txt", "a")
+                    break
+    new_face = []
     for i in face:
         x, y, w, h = i
         face_cropped = image_GREY[y:y+h, x:x+w]
-        thresholded = cv2.adaptiveThreshold(face_cropped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11, 5)
-        thresholded = cv2.resize(thresholded, (64, 64))
-        for secondLVL in thresholded:
-            file.write(str(secondLVL))
+        thresholded = cv2.resize(face_cropped, (64, 64))
+        thresholded = cv2.adaptiveThreshold(thresholded, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 4)
+        new_face.append(thresholded)
+    return new_face
+
+new_face = get_face()
+file = open("database.txt", "a")
+for trueVAL in new_face:
+     np.savetxt(file, trueVAL, delimiter=",")
+     file.write("------------------------\n")
+file.close()
