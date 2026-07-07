1d6ae2c43d4e55ecabd83e41971e81cfd645fc45
Shok-king <ayaan106643@gmail.com>
2026-07-07 23:09:04 +0100

    Refactor LiveAuthentication for improved face detection
    Refactored face detection logic and currently adding similarity check function instead of a hard check of yes/no.

--- LiveAuthentication.py
+++ LiveAuthentication.py
@@ -2,24 +2,10 @@ import cv2
 from pykinect2 import PyKinectV2
 from pykinect2 import PyKinectRuntime
 import numpy as np 
-from Reg_NEWUSER import get_face, new_face
-from Reg_NEWUSER import file
+import time
+from Reg_NEWUSER import get_face
 
-database = open(file, 'r')
-database = np.loadtxt(database, delimiter= ",")
-facedetect = False
-while not facedetect:
-    get_face()
-    for compare_data in new_face:
-        for stored_data in database:
-            if compare_data == stored_data:
-                print("Face matched")
-                facedetect = True
-                import time
-                seconds = 3
-                if time.time() > seconds:
-                    facedetect = False
-            else:
-                print("face not matched")
-
-
+face = get_face()
+def similaritycheck():
+    file = open("database.txt", "r")
+    for everyline in file:
