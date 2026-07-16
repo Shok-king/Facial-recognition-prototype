4ef6cb12c2dd8083da8978547525ce9f7cd9661a
Shok-king <ayaan106643@gmail.com>
2026-07-16 01:06:32 +0100

    Implement face recognition logic in LiveAuthentication.py
    Added Hamming distance function and conversion logic for face recognition.

--- LiveAuthentication.py
+++ LiveAuthentication.py
@@ -5,7 +5,39 @@ import numpy as np
 import time
 from Reg_NEWUSER import get_face
 
-face = get_face()
-def similaritycheck():
+import time
+
+def hamming(stored_position, recieved_position):
+    counter = 0
+    for x, y in zip(stored_position, recieved_position):
+        if x != y:
+            counter += 1
+    return counter
+
+def conversion(everyline):
+        parts = everyline.split(":")
+        NumberVAR = parts[1].split(",")
+        convert = []
+        for v in NumberVAR:
+            v = int(v)
+            convert.append(v)
+        return parts[0], convert
+
+while True:
+    print("Warming up camera module...")
+    time.sleep(2)
+    liveface = get_face()
+    Matched = False
     file = open("database.txt", "r")
     for everyline in file:
+        ID_name, convert = conversion(everyline)
+        counter = hamming(convert, liveface)
+    
+        if counter <= 500:
+            print(f"face matched welcome {ID_name}")
+            Matched = True
+            break
+    else:
+        print("Match not found rejected.")
+     
+    file.close()
